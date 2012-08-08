9ab9f27272d8f93779e79f35a551d11986464ae4
Ben Holloway <ben@holloway.gb.com>
2012-08-08 10:04:42 +0100

    Script changes

--- script.js
+++ script.js
@@ -28,18 +28,27 @@ function loadcards(){
 				id: 1,
 				country_name: 'Canada',
 				country_code: 'CA',
-				continent: 'Americas'
+				continent: 'Americas',
+				gold: '6',
+				silver: '12',
+				bronze: '2'
 			},
 			{
 				id: 2,
 				country_name: 'Great Britain',
 				country_code: 'GB',
-				continent: 'Europe'
+				continent: 'Europe',
+				gold: '11',
+				silver: '10',
+				bronze: '29'
 			},
 			{	id: 3,
 				country_name: 'Germany',
 				country_code: 'DE',
-				continent: 'Europe'
+				continent: 'Europe',
+				gold: '3',
+				silver: '9',
+				bronze: '7'
 			}
 		],
 		opponent: [
@@ -47,20 +56,28 @@ function loadcards(){
 				id: 4,
 				country_name: 'North Korea',
 				country_code: 'NK',
-				continent: 'Asia'
+				continent: 'Asia',
+				gold: '3',
+				silver: '3',
+				bronze: '7'
 			},
 			{
 				id: 5,
 				country_name: 'Belize',
 				country_code: 'BL',
-				continent: 'Asia'
-
+				continent: 'Asia',
+				gold: '9',
+				silver: '13',
+				bronze: '8'
 			},
 			{	
 				id: 6,
 				country_name: 'Bermuda (Overseas Territory)',
 				country_code: 'BU',
-				continent: 'Americas'
+				continent: 'Americas',
+				gold: '20',
+				silver: '21',
+				bronze: '2'
 			}
 		]
 	};
@@ -71,9 +88,10 @@ var rannum = Math.floor(Math.random()*4);
 
 	$.each(cards.you, function(index, value){
 		$('section.data').append('<ul>');
-   		$('section.data').append('<li>' + value.country_name + '</li>');
-   		$('section.data').append('<li>' + value.country_code + '</li>');
-   		$('section.data').append('<li>' + value.continent + '</li>');
+   		$('section.data').append('<li>' + value.country_name + ', ' + value.country_code + ', ' + value.continent + '.</li>');
+   		$('section.data').append('<li>Gold: ' + value.gold + '</li>');
+   		$('section.data').append('<li>Silver: ' + value.silver + '</li>');
+   		$('section.data').append('<li>Bronze: ' + value.bronze + '</li>');
    		$('section.data').append('</ul>');
    		$('section.data').append('<br>');
 
