495077a9d36fdea7b10aeb0fbc34a8eae14a5ac7
Joshua Goodwin <j@joshuagoodwin.com>
2012-08-08 16:14:46 +0100

    FINALLY: cards never repeated in a session
    IT WORKS!!!!!!!!!!!!!!!1111111111111111111111111

--- php/newcards.json.php
+++ php/newcards.json.php
@@ -2,6 +2,7 @@
 require 'confidential_credentials.php';
 header('Content-Type: application/json; charset=utf-8');
 header('Access-Control-Allow-Origin: http://localhost/, http://yrsolympics2.phpfogapp.com/');
+@session_start();
 
 $hand_size = 4; // default
 if (isset($_GET['hand_size'])) {
@@ -15,14 +16,14 @@ mysql_select_db($dbname) or die(mysql_error());
 
 $query = "SELECT * FROM cards";
 if (isset($_SESSION['sofar'])) {
-    $query .= "WHERE id!='103'";
+    $query .= " WHERE id NOT IN ('";
+    $query .= implode( "','" , $_SESSION['sofar'] ) . "')";
 }
 $query .= " ORDER BY RAND() LIMIT ";
 $query .= $hand_size;
 
+// echo $query;
 $result = mysql_query($query) or die(mysql_error());
-
-// ^ To do: exclude previously spat cards from being selected again ("NOT LIKE"?)
 
 $hand = $sofar = array();
 
@@ -31,13 +32,11 @@ while( $row = mysql_fetch_array($result) ) {
     array_push($sofar, $row['id']);
 }
 
-$json = json_encode($hand);
+$json = json_encode($hand,JSON_PRETTY_PRINT);
 
 echo isset($_GET['callback'])
     ? "{$_GET['callback']}($json)"
     : $json;
-
-@session_start();
 
 if (!isset($_SESSION['sofar'])) {
     $_SESSION['sofar'] = $sofar;
@@ -45,7 +44,8 @@ if (!isset($_SESSION['sofar'])) {
     $_SESSION['sofar'] = array_merge($_SESSION['sofar'], $sofar);
 }
 
-echo "\n/*\n";
-echo var_dump($_SESSION['sofar']);
-echo "*/\n";
+// echo "\n/*\nso josh knows what he's doing:\n\n";
+// echo $query . "\n\n";
+// echo "id='" . implode("' id!='",$_SESSION['sofar']) . "'";
+// echo "*/\n";
 ?>
