2b64b6a27fb0ba96ac28a1d3005df782dd338eee
Ben Holloway <ben@holloway.gb.com>
2012-08-09 16:02:04 +0100

    Grapical changes, scores.

--- script.js
+++ script.js
@@ -63,7 +63,12 @@ $(document).ready(function () {
 			else{
 			$('.result').append('<p>Tie!</p>')
 		}
+
+		$('.resulta').html(game.score.mine/100);
+		$('.resultb').html(game.score.yours/100);
 	});
+
+
 
 	$('body').delegate('#mine .data li', 'click', function(evt){
 		var el = $(evt.currentTarget),
@@ -88,14 +93,14 @@ $(document).ready(function () {
 
 	});
 
-	 /* $.ajax({
+	  $.ajax({
 		url:'./php/newcards.json.php', 
 		data: {hand_size: HAND_SIZE*2},
 		success: deal,
 		dataType: 'jsonp'
-	}); */
+	}); 
 
-     deal([{"0":"112","id":"112","1":"eg","country_code":"eg","2":"Egypt","country_name":"Egypt","3":"0","medals_gold":"0","4":"1","medals_silver":"1","5":"0","medals_bronze":"0","6":"120","endangered_species":"120","7":"468500","military_personnel":"468500"},{"0":"95","id":"95","1":"ch","country_code":"ch","2":"Switzerland","country_name":"Switzerland","3":"1","medals_gold":"1","4":"1","medals_silver":"1","5":"0","medals_bronze":"0","6":"48","endangered_species":"48","7":"22823","military_personnel":"22823"},{"0":"108","id":"108","1":"id","country_code":"id","2":"Indonesia","country_name":"Indonesia","3":"0","medals_gold":"0","4":"1","medals_silver":"1","5":"1","medals_bronze":"1","6":"1126","endangered_species":"1126","7":"302000","military_personnel":"302000"},{"0":"74","id":"74","1":"kp","country_code":"kp","2":"Democratic People's Republic of Korea","country_name":"Democratic People's Republic of Korea","3":"4","medals_gold":"4","4":"0","medals_silver":"0","5":"1","medals_bronze":"1","6":"48","endangered_species":"48","7":"110600","military_personnel":"110600"},{"0":"64","id":"64","1":"cn","country_code":"cn","2":"People's Republic of China","country_name":"People's Republic of China","3":"31","medals_gold":"31","4":"17","medals_silver":"17","5":"14","medals_bronze":"14","6":"841","endangered_species":"841","7":"218500","military_personnel":"218500"},{"0":"96","id":"96","1":"ir","country_code":"ir","2":"Islamic Republic of Iran","country_name":"Islamic Republic of Iran","3":"1","medals_gold":"1","4":"0","medals_silver":"0","5":"1","medals_bronze":"1","6":"86","endangered_species":"86","7":"523000","military_personnel":"523000"},{"0":"117","id":"117","1":"gr","country_code":"gr","2":"Greece","country_name":"Greece","3":"0","medals_gold":"0","4":"0","medals_silver":"0","5":"2","medals_bronze":"2","6":"131","endangered_species":"131","7":"156600","military_personnel":"156600"},{"0":"77","id":"77","1":"nz","country_code":"nz","2":"New Zealand","country_name":"New Zealand","3":"3","medals_gold":"3","4":"0","medals_silver":"0","5":"4","medals_bronze":"4","6":"150","endangered_species":"150","7":"9278","military_personnel":"9278"},{"0":"113","id":"113","1":"gt","country_code":"gt","2":"Guatemala","country_name":"Guatemala","3":"0","medals_gold":"0","4":"1","medals_silver":"1","5":"0","medals_bronze":"0","6":"233","endangered_species":"233","7":"15500","military_personnel":"15500"},{"0":"66","id":"66","1":"gb","country_code":"gb","2":"Great Britain","country_name":"Great Britain","3":"16","medals_gold":"16","4":"11","medals_silver":"11","5":"10","medals_bronze":"10","6":"72","endangered_species":"72","7":"160280","military_personnel":"160280"}]);
+    // deal([{"0":"112","id":"112","1":"eg","country_code":"eg","2":"Egypt","country_name":"Egypt","3":"0","medals_gold":"0","4":"1","medals_silver":"1","5":"0","medals_bronze":"0","6":"120","endangered_species":"120","7":"468500","military_personnel":"468500"},{"0":"95","id":"95","1":"ch","country_code":"ch","2":"Switzerland","country_name":"Switzerland","3":"1","medals_gold":"1","4":"1","medals_silver":"1","5":"0","medals_bronze":"0","6":"48","endangered_species":"48","7":"22823","military_personnel":"22823"},{"0":"108","id":"108","1":"id","country_code":"id","2":"Indonesia","country_name":"Indonesia","3":"0","medals_gold":"0","4":"1","medals_silver":"1","5":"1","medals_bronze":"1","6":"1126","endangered_species":"1126","7":"302000","military_personnel":"302000"},{"0":"74","id":"74","1":"kp","country_code":"kp","2":"Democratic People's Republic of Korea","country_name":"Democratic People's Republic of Korea","3":"4","medals_gold":"4","4":"0","medals_silver":"0","5":"1","medals_bronze":"1","6":"48","endangered_species":"48","7":"110600","military_personnel":"110600"},{"0":"64","id":"64","1":"cn","country_code":"cn","2":"People's Republic of China","country_name":"People's Republic of China","3":"31","medals_gold":"31","4":"17","medals_silver":"17","5":"14","medals_bronze":"14","6":"841","endangered_species":"841","7":"218500","military_personnel":"218500"},{"0":"96","id":"96","1":"ir","country_code":"ir","2":"Islamic Republic of Iran","country_name":"Islamic Republic of Iran","3":"1","medals_gold":"1","4":"0","medals_silver":"0","5":"1","medals_bronze":"1","6":"86","endangered_species":"86","7":"523000","military_personnel":"523000"},{"0":"117","id":"117","1":"gr","country_code":"gr","2":"Greece","country_name":"Greece","3":"0","medals_gold":"0","4":"0","medals_silver":"0","5":"2","medals_bronze":"2","6":"131","endangered_species":"131","7":"156600","military_personnel":"156600"},{"0":"77","id":"77","1":"nz","country_code":"nz","2":"New Zealand","country_name":"New Zealand","3":"3","medals_gold":"3","4":"0","medals_silver":"0","5":"4","medals_bronze":"4","6":"150","endangered_species":"150","7":"9278","military_personnel":"9278"},{"0":"113","id":"113","1":"gt","country_code":"gt","2":"Guatemala","country_name":"Guatemala","3":"0","medals_gold":"0","4":"1","medals_silver":"1","5":"0","medals_bronze":"0","6":"233","endangered_species":"233","7":"15500","military_personnel":"15500"},{"0":"66","id":"66","1":"gb","country_code":"gb","2":"Great Britain","country_name":"Great Britain","3":"16","medals_gold":"16","4":"11","medals_silver":"11","5":"10","medals_bronze":"10","6":"72","endangered_species":"72","7":"160280","military_personnel":"160280"}]);
 
 });
 
