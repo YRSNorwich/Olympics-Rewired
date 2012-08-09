1cf729e15fb32254a0352c97986994ec42a9f0b7
Joshua Goodwin <j@joshuagoodwin.com>
2012-08-09 15:28:16 +0100

    It actually keeps the score!

--- php/comparecards.php
+++ php/comparecards.php
@@ -1,51 +1,31 @@
 <?php
 header('content-type: application/json');
 session_start();
-
 if (!isset($_SESSION['score'])) {
-    $_SESSION['score'] = array('mine' => 0, 'yours' => 0);
+    $_SESSION['score'] = array();
 }
 
-// require 'confidential_credentials.php';
-// mysql_connect($host,$user,$pass) or die(mysql_error());
-// mysql_select_db($dbname) or die(mysql_error());
-// result({mine:1, yours:0, lastWinner:'mine'});
-
 $compare = $_GET['chosenProperty'];
-$card_mine = $_GET['mine']; //
+$card_mine = $_GET['mine'];
 $card_yours = $_GET['yours'];
 
-// echo '/*
-// ';
-// echo var_dump($_GET);
-// echo '*/';
+if ( !settype($card_mine[$compare],'integer') or !settype($card_yours[$compare],'integer') ){
+    $card_mine[$compare] = strlen($card_mine[$compare]);
+    $card_mine[$compare] = strlen($card_mine[$compare]);
+}
 
-// $player_card = 'gb';
-// mysql_query();
-
-// if ( !settype($card_mine[$compare],'integer') or !settype($card_yours[$compare],'integer') )
-// {
-//     $card_mine[$compare] = strlen($card_mine[$compare]);
-//     $card_mine[$compare] = strlen($card_mine[$compare]);
-// }
-
-$score_mine = $score_yours = 0;
 if ( $card_mine[$compare] > $card_yours[$compare] )
 {
-   $_SESSION['score']['mine'] .+ 100;
-   // $_SESSION['score']['yours'] 1;
+   $_SESSION['score']['mine'] += 100;
    $lastWinner = 'me';
 }
 elseif ( $card_mine[$compare] < $card_yours[$compare] )
 {
-    // $_SESSION['score']['mine'] .- 1;
-    $_SESSION['score']['yours'] .+ 100;
+    $_SESSION['score']['yours'] += 100;
     $lastWinner = 'you';
 }
 else
 {
-    $_SESSION['score']['mine'] .+ 0;
-    $_SESSION['score']['yours'] .+ 0;
     $lastWinner = 'tie';
 }
 
