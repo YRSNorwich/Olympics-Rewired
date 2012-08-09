39d2bbd70a627a2d0eede54dca9e7bdaff79f5e9
Joshua Goodwin <j@joshuagoodwin.com>
2012-08-09 11:32:29 +0100

    Beginnings of comparison machine

--- php/comparecards.php
+++ php/comparecards.php
@@ -3,19 +3,13 @@ require 'confidential_credentials.php';
 mysql_connect($host,$user,$pass) or die(mysql_error());
 mysql_select_db($dbname) or die(mysql_error());
 
-// $player_card = $_GET['player'];
-// $opponent_card = $_GET['opponent'];
+echo var_dump($_GET[]);
+
+// $player_card = $_GET['mine'];
+// $opponent_card = $_GET['yours'];
 
 $player_card = 'gb';
-
-
-// Not right - it doesn't matter if the winner 
-
-if $player > $opponent {
-    $points_player += 1; // Player wins a point
-} elseif $player < $opponent {
-    $points_opponent += 1; // Opponent wins a point
-}
+mysql_query();
 
 
 ?>
